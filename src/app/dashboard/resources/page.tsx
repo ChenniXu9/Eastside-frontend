2868887cfd42c6f9b8782c028124db1625606577
Cloudy <mac@Cloudy.local>
2024-07-24 15:33:18 -0700

    AddCourse, Archive button for resources library

--- src/app/dashboard/resources/page.tsx
+++ src/app/dashboard/resources/page.tsx
@@ -5,37 +5,43 @@ import { useRouter } from 'next/navigation';
 import CourseCard from '../../../components/resources/CourseCard';
 import AddCourse from '../../../components/resources/AddCourse';
 import EditCourse from '../../../components/resources/EditCourse';
+import ArchiveCourses from '../../../components/resources/ArchiveCourse';
 
 const initialCourses = [
   {
     courseCode: "1",
     courseName: "Adaptive Leadership program",
     semester: "Class of 2025",
-    courseFrontpage: "https://uploads.visitseattle.org/2023/01/11122537/Banner_rachael-jones-media_aerial-destination-photos-24_3.jpg"
+    courseFrontpage: "https://uploads.visitseattle.org/2023/01/11122537/Banner_rachael-jones-media_aerial-destination-photos-24_3.jpg",
+    archived: false
   },
   {
     courseCode: "2",
     courseName: "Adaptive Leadership program",
     semester: "Class of 2024",
-    courseFrontpage: "https://media.tacdn.com/media/attractions-splice-spp-674x446/06/71/21/da.jpg"
+    courseFrontpage: "https://media.tacdn.com/media/attractions-splice-spp-674x446/06/71/21/da.jpg",
+    archived: false
   },
   {
     courseCode: "3",
     courseName: "Executive Insight",
     semester: "Spring 2024",
-    courseFrontpage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV2-qJuHx3GgYorwKuGrRaqz-GNgB8MMWkhg&s"
+    courseFrontpage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV2-qJuHx3GgYorwKuGrRaqz-GNgB8MMWkhg&s",
+    archived: false
   },
   {
     courseCode: "4",
     courseName: "Executive Insight",
     semester: "Fall 2023",
-    courseFrontpage: "https://media.tacdn.com/media/attractions-splice-spp-674x446/06/71/21/da.jpg"
+    courseFrontpage: "https://media.tacdn.com/media/attractions-splice-spp-674x446/06/71/21/da.jpg",
+    archived: false
   },
   {
     courseCode: "5",
     courseName: "Executive Insight",
     semester: "Spring 2023",
-    courseFrontpage: "https://uploads.visitseattle.org/2023/01/11122537/Banner_rachael-jones-media_aerial-destination-photos-24_3.jpg"
+    courseFrontpage: "https://uploads.visitseattle.org/2023/01/11122537/Banner_rachael-jones-media_aerial-destination-photos-24_3.jpg",
+    archived: false
   },
 ];
 
@@ -44,6 +50,8 @@ const ResourcePageContent: React.FC = () => {
   const [isAddingCourse, setIsAddingCourse] = useState(false);
   const [isEditingCourse, setIsEditingCourse] = useState(false);
   const [editingCourse, setEditingCourse] = useState(null);
+  const [isArchiving, setIsArchiving] = useState(false);
+  const [selectedCourses, setSelectedCourses] = useState<string[]>([]);
   const router = useRouter();
 
   const handleAddCourse = (newCourse) => {
@@ -72,19 +80,43 @@ const ResourcePageContent: React.FC = () => {
     setIsEditingCourse(false);
   };
 
+  const handleArchive = () => {
+    setIsArchiving(true);
+  };
+
+  const handleArchiveConfirm = (selectedCourses: string[]) => {
+    setCourses(courses.map(course => 
+      selectedCourses.includes(course.courseCode) ? { ...course, archived: true } : { ...course, archived: false }
+    ));
+    setSelectedCourses(selectedCourses);
+    setIsArchiving(false);
+  };
+
+  const handleArchiveCancel = () => {
+    setIsArchiving(false);
+  };
+
   return (
     <div className="flex flex-wrap justify-center gap-4 p-4 bg-white">
       {courses.map((course, index) => (
-        <CourseCard
-          key={index}
-          courseCode={course.courseCode}
-          courseName={course.courseName}
-          semester={course.semester}
-          courseFrontpage={course.courseFrontpage}
-          onEditCourse={handleEditCourse} 
-        />
+        !course.archived && (
+          <CourseCard
+            key={index}
+            courseCode={course.courseCode}
+            courseName={course.courseName}
+            semester={course.semester}
+            courseFrontpage={course.courseFrontpage}
+            onEditCourse={handleEditCourse} 
+          />
+        )
       ))}
       <div className="w-full flex justify-center mt-8">
+        <button
+          onClick={handleArchive}
+          className="bg-blue-600 mr-10 text-white py-2 px-6 rounded-full transition duration-300 ease-in-out hover:bg-blue-700 transform hover:-translate-y-1"
+        >
+          Archive
+        </button>
         <button
           onClick={handleAddCourseButton}
           className="bg-blue-800 text-white py-2 px-6 rounded-full transition duration-300 ease-in-out hover:bg-blue-700 transform hover:-translate-y-1"
@@ -107,6 +139,14 @@ const ResourcePageContent: React.FC = () => {
             onSaveCourse={handleSaveCourse}
           />
         </div>
+      )}
+      {isArchiving && (
+        <ArchiveCourses
+          courses={courses}
+          selectedCourses={selectedCourses}
+          onArchive={handleArchiveConfirm}
+          onCancel={handleArchiveCancel}
+        />
       )}
     </div>
   );
