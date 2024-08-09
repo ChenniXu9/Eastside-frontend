9ef30baabcc6a265947ce2d96ee24cf8ae66cb0f
takaraktruong <truong.ta@northeastern.edu>
2024-08-08 19:54:50 -0700

    completed styling

--- src/components/UpdateUser.tsx
+++ src/components/UpdateUser.tsx
@@ -83,10 +83,11 @@ const UpdateUser = ({ user }: { user: User }) => {
                 Update your Information
             </span>
             {open && (
-                <div className="absolute w-screen h-screen top-0 left-0 bg-black bg-opacity-65 flex items-center justify-center z-50">
+                <div className="absolute w-screen h-screen top-0 left-0 bg-black bg-opacity-65 flex items-center justify-center z-50 ">
                     <form
                         onSubmit={handleSubmit}
-                        className="p-2 md:p-12 bg-white rounded-lg shadow-md flex flex-col items-center gap-2 w-full md:w-1/2 xl:w-1/3 relative text-center"
+                        // p-12 bg-white rounded-lg shadow-md flex flex-col gap-2 w-full md:w-1/2 xl:w-1/3 relative text-center
+                        className="p-2 md:p-12 bg-white rounded-lg shadow-md flex flex-col gap-2 w-full md:w-1/2 xl:w-1/3 relative text-center"
                     >
                         <h1 className="text-xl">Update Profile</h1>
                         <div className="flex flex-row justify-evenly">
@@ -346,7 +347,7 @@ const UpdateUser = ({ user }: { user: User }) => {
                                                 ? "text"
                                                 : "password"
                                         }
-                                        placeholder="Confirm your password"
+                                        placeholder="Confirm password"
                                         className="ring-1 ring-gray-300 p-[13px] rounded-md text-sm"
                                         name="confirm_password"
                                         id="confirm_password"
