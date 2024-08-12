c78de7cc3041435fa5d10c4e480d71b0ab7743ff
ChenniXu9 <143971914+ChenniXu9@users.noreply.github.com>
2024-08-11 22:09:17 -0700

    Update page.tsx

--- src/app/dashboard/channels/members_detail/[channelName]/[username]/page.tsx
+++ src/app/dashboard/channels/members_detail/[channelName]/[username]/page.tsx
@@ -1,17 +1,9 @@
 "use client";
 import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
-<<<<<<< HEAD:src/app/dashboard/channels/members_detail/[channelName]/[username]/page.tsx
 import Members from "@/components/channelContents/Members";
 import MembersRight from "@/components/channelContents/MembersRight";
 
-=======
-import LeftMenu from "@/components/LeftMenu";
-import ChannelNavbar from "@/components/channelContents/ChannelNavbar";
-import MyPostsRightbar from "@/components/channelContents/MyPostsRightbar";
-import UserDetail from "@/components/channelContents/PostsDetail";
-import { deletePost, fetchUserPosts } from '@/lib/actions';
->>>>>>> main:src/app/dashboard/channels/posts_detail/[id]/[userId]/page.tsx
 
 type User = {
   id: string;
@@ -63,20 +55,13 @@ type Channel = {
 };
 
 
-<<<<<<< HEAD:src/app/dashboard/channels/members_detail/[channelName]/[username]/page.tsx
 const GroupDetail = () => {
-=======
-
-const PostsDetail = () => {
->>>>>>> main:src/app/dashboard/channels/posts_detail/[id]/[userId]/page.tsx
   const params = useParams();
   const channelName = params?.channelName as string | undefined;
   const userName = params?.username as string;
 
   const [channel, setChannel] = useState<Channel | null>(null);
   const [currentUser, setCurrentUser] = useState<User | null>(null);
-
-  const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
     if (channelName && userName) {
@@ -98,36 +83,16 @@ const PostsDetail = () => {
     }
   }, [channelName, userName]);
 
-  useEffect(() => {
-    const loadPosts = async () => {
-      if (channel?.id && currentUser) {
-        const fetchedPosts = await fetchUserPosts(channel.id, currentUser.id);
-        setPosts(fetchedPosts);
-      }
-    };
-
-    loadPosts();
-  }, [channel?.id, currentUser]);
-
-
   if (!channel || !currentUser) return <div>Loading...</div>;
 
   const hasJoined = channel.users.some(user => user.user.id === currentUser.id);
   
   return (
-<<<<<<< HEAD:src/app/dashboard/channels/members_detail/[channelName]/[username]/page.tsx
     <div>
       <div className="flex gap-6 pt-6">
         <div className="w-full lg:w-[70%] xl:w-[70%]">
           {/* <Members /> */}
           <Members channel={channel} currentUser={currentUser}/>
-=======
-    <div className='text-black '>
-      {/* <div><ChannelNavbar channel={channel} currentUser={currentUser}/></div> */}
-      <div className="flex gap-6 pt-6">
-        <div className="w-full lg:w-[70%] xl:w-[70%]">
-          <UserDetail channel={channel} currentUser={currentUser} posts={posts} setPosts={setPosts}/>
->>>>>>> main:src/app/dashboard/channels/posts_detail/[id]/[userId]/page.tsx
         </div>
         <div className="hidden lg:block w-[30%]"><MembersRight channel={channel} currentUser={currentUser}/></div>
       </div>
